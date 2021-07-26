c4c3adcbe7db659169ddd963598694f30626c61b
Sarcoma <sarcoma@live.co.uk>
2021-07-26 01:00:02 +0100

    getStaticProps

--- components/gallery-section.js
+++ components/gallery-section.js
@@ -4,7 +4,7 @@ const GallerySection = ({section}) =>
     <section>
         <h2>{section.title}</h2>
         <div className="row">
-            {section.items.map(item => <GalleryItem item={item} />)}
+            {section.items.map(item => <GalleryItem key={item.id} item={item} />)}
         </div>
     </section>;
 
--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,19 @@ import galleryData from '../data/gallery-data';
 import GallerySection from '../components/gallery-section';
 import getObjktData from '../api/get-objkt-data';
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
   const objkts = await getObjktData();
   const gallerySections = galleryData.map(({title, items}) => ({title, items: items.map(id => (objkts[id]))}))
   return {
-    props: {gallerySections}, // will be passed to the page component as props
-    revalidate: 900,
+    props: {gallerySections},
+    revalidate: 300,
   }
 }
 
 export default function Home({gallerySections}) {
   return (
     <>
-      {gallerySections.map(section => <GallerySection section={section} />)}
+      {gallerySections.map(section => <GallerySection key={section.title} section={section} />)}
     </>
   )
 }
