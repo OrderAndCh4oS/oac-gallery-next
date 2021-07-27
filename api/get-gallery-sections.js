20b87d5716e76e519f7276543282d0691d4ee3d2
Sarcoma <sarcoma@live.co.uk>
2021-07-27 07:46:53 +0100

    Add filter

--- api/get-gallery-sections.js
+++ api/get-gallery-sections.js
@@ -1,4 +1,5 @@
 import { gql, request } from 'graphql-request';
+import galleryData from '../data/gallery-data';
 
 const query = gql`
     query ObjktsWithPrice($addresses: [String!]) {
@@ -24,15 +25,15 @@ const query = gql`
     }`;
 
 const variables = {
-    'addresses': [
-        'tz1VgpmwW66LCbskjudK54Zp96vKn2cHjpGN',
-        'tz1KySTBB8RXWVraggfXWLaLR9H3K3JBEbgt',
-        'KT1HbQepzV1nVGg8QVznG7z4RcHseD5kwqBn',
-        'KT1FvqJwEDWb1Gwc55Jd1jjTHRVWbYKUUpyq',
+    "addresses": [
+        "tz1VgpmwW66LCbskjudK54Zp96vKn2cHjpGN",
+        "tz1KySTBB8RXWVraggfXWLaLR9H3K3JBEbgt",
+        "KT1HbQepzV1nVGg8QVznG7z4RcHseD5kwqBn",
+        "KT1FvqJwEDWb1Gwc55Jd1jjTHRVWbYKUUpyq",
     ],
 };
 
-function parseData(obj, objkt) {
+const parseObjkt = (obj, objkt) => {
     const supply = objkt.supply;
     const available = objkt.token_holders.reduce((sum, th) => sum + th.quantity, 0);
     const notForSaleText = available === 0 ? 'SOLD' : 'NOT LISTED';
@@ -40,11 +41,16 @@ function parseData(obj, objkt) {
     const availability = `${available}/${supply}`;
 
     return {...obj, [objkt.id]: {...objkt, price, availability}};
-}
-
-const getObjktData = async() => {
-    const response = await request('https://api.hicdex.com/v1/graphql', query, variables);
-    return response?.hic_et_nunc_token.reduce(parseData, {}) || {};
 };
 
-export default getObjktData;
+const parseObjktsResponse = (response) => {
+    let parsedResponse = response?.hic_et_nunc_token.reduce(parseObjkt, {}) || {};
+    return galleryData.map(({title, items}) => ({title, items: items.map(id => (parsedResponse[id]))}))
+}
+
+const getGallerySections = async() => {
+    const response = await request('https://api.hicdex.com/v1/graphql', query, variables);
+    return parseObjktsResponse(response);
+};
+
+export default getGallerySections;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,38 @@
-import galleryData from '../data/gallery-data';
 import GallerySection from '../components/gallery-section';
-import getObjktData from '../api/get-objkt-data';
+import getGallerySections from '../api/get-gallery-sections';
+import { useState } from 'react';
 
 export async function getStaticProps(context) {
-  const objkts = await getObjktData();
-  const gallerySections = galleryData.map(({title, items}) => ({title, items: items.map(id => (objkts[id]))}))
-  return {
-    props: {gallerySections},
-    revalidate: 300,
-  }
+    const gallerySections = await getGallerySections();
+
+    return {
+        props: {gallerySections},
+        revalidate: 300,
+    };
 }
 
 export default function Home({gallerySections}) {
-  return (
-    <>
-      {gallerySections.map(section => <GallerySection key={section.title} section={section} />)}
-    </>
-  )
+    const [filteredSections, setFilteredSections] = useState(gallerySections);
+
+    const handleFilter = (event) => {
+        const filter = event.target.value;
+        const nextFilteredSections = filter === 'for-sale'
+            ? gallerySections
+                .filter(gs => gs.items.some(i => i.swaps.length))
+                .map(gs => ({...gs, items: gs.items.filter(i => i.swaps.length)}))
+            : gallerySections;
+        setFilteredSections(nextFilteredSections);
+    };
+
+    return (
+        <>
+            <label htmlFor='filter' className='label'>Filter</label>
+            <select onChange={handleFilter} className='select' id='filter'>
+                <option value={'all'}>All</option>
+                <option value={'for-sale'}>For Sale</option>
+            </select>
+            {filteredSections.map(
+                section => <GallerySection key={section.title} section={section}/>)}
+        </>
+    );
 }
