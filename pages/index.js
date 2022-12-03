22d397b63a4cdd559c78b1142735c2a43a95ea3b
order&chaos <sean@orderandchaoscreative.com>
2022-12-03 13:25:37 +0000

    rm filters

--- pages/index.js
+++ pages/index.js
@@ -17,31 +17,6 @@ export async function getStaticProps(context) {
     };
 }
 
-function filterProjects(filter, gallerySections) {
-    return filter !== 'all'
-        ? gallerySections.filter(gs => gs.title === filter)
-        : gallerySections;
-}
-
-function filterSections(filter, gallerySections) {
-    return filter === 'for-sale'
-        ? gallerySections
-            .filter(gs => gs.items.some(i => i.swaps.length))
-            .map(gs => ({...gs, items: gs.items.filter(i => i.swaps.length)}))
-        : gallerySections;
-}
-
-function sortSections(sort, filteredSections) {
-    return filteredSections.map(fs => ({
-        ...fs,
-        items: fs.items.sort((a, b) => {
-            let minA = Math.min(...a.swaps.map(swap => swap.price));
-            let minB = Math.min(...b.swaps.map(swap => swap.price));
-            return sort === 'low-to-high' ? minA - minB : minB - minA;
-        })
-    }));
-}
-
 export default function Home({gallerySections}) {
     const [sections, setSections] = useState(gallerySections);
     const projectRef = createRef();
@@ -62,49 +37,6 @@ export default function Home({gallerySections}) {
 
     return (
         <>
-            <div className={styles.filterBar}>
-                <div>
-                    <label htmlFor="filter" className="label">Filter</label>
-                    <select
-                        onChange={handleChange}
-                        className="select"
-                        id="filter"
-                        ref={filterRef}
-                    >
-                        <option value={'all'}>All</option>
-                        <option value={'for-sale'}>For Sale</option>
-                    </select>
-                </div>
-                <div className={styles.marginLeft}>
-                    <label htmlFor="sort" className="label">Sort (per
-                                                            collection)</label>
-                    <select
-                        onChange={handleChange}
-                        className="select"
-                        id="sort"
-                        ref={sortRef}
-                    >
-                        <option value={'none'}>None</option>
-                        <option value={'low-to-high'}>Low to High</option>
-                        <option value={'high-to-low'}>High to Low</option>
-                    </select>
-                </div>
-                <div className={styles.marginLeft}>
-                    <label htmlFor="project" className="label">Project</label>
-                    <select
-                        onChange={handleChange}
-                        className="select"
-                        id="project"
-                        ref={projectRef}
-                    >
-                        <option value={'all'}>All</option>
-                        {gallerySections.map(section => <option
-                            key={`${section.title}_opt`}
-                            value={section.title}
-                        >{section.title}</option>)}
-                    </select>
-                </div>
-            </div>
             {sections.map((section, i) => <GallerySection
                 key={`${section.title}_${i}`}
                 section={section}
